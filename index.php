0c62c3d8c6fc8b48c1b57a5c66d8441db9e45688
Lulu Ngei <ngeilulu@gmail.com>
2017-03-05 17:43:04 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,61 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+       
+    <title>e-Books</title>  
+    <link href="styles.css" rel="stylesheet">  
+</head>
+
+<body >
+<header>
+<a href= "login.php" Login></a>
+</header>
+<section>
+<h1> Books<h2>
+<p>"Book are the quietest and nost constant of friends; they are the most accessible and
+wisest of counselors, and the most patient of teachers."</p>
+
+<p>-Charles William Elliot</p>
+
+<form class="col-sm-6" method="POST">
+                        <h3>New User</h3>
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input name="name" type="text" class="form-control" placeholder="Enter your name" required>
+                        </div>
+                        <div class="form-group">
+                            <label>E-mail</label>
+                            <input name="E-mail" type="text" class="form-control" placeholder="Enter your E-mail address" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Username</label>
+                           
+                            <input name="userName" type="text" class="form-control" placeholder="Enter your username">
+                            
+                       
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input name="password" type="text" class="form-control" placeholder="Enter new password">
+                            </div>
+
+                            <div class="form-group">
+                            <label>Re-enter password</label>
+                            <input name="password" type="text" class="form-control" placeholder="Re-enter password">
+                            </div>
+                        <div class="form-group">
+                            <button class="btn btn-success form-control">Sign In</button>
+                        </div>    
+                    </form>
+</section>
+
+
+
+
+
+     
+</body>
+</html>
+
+
