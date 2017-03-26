0a5ea73c59446ed39d9c6ef280ceb8a926c40841
Clement Habinshuti <hello@habbes.xyz>
2017-03-26 23:20:55 +0300

    Update database schema

--- sql/ebooks.sql
+++ sql/ebooks.sql
@@ -1,100 +1,86 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.16  Distrib 10.1.21-MariaDB, for Linux (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 26, 2017 at 04:04 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: localhost
+-- ------------------------------------------------------
+-- Server version	10.1.21-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `ebooks`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `books`
 --
 
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(250) NOT NULL,
   `genre` varchar(250) NOT NULL,
   `author` varchar(250) NOT NULL,
   `year` int(11) NOT NULL,
-  `price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `price` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`title`, `genre`, `author`, `year`, `price`) VALUES
-('A Human Element book 1', 'Thriller', 'Donna Gallanti', 2012, 150),
-('A Human Element book 2', 'Thriller', 'Donna Galanti', 2013, 150),
-('A Walk to Remember', 'Romance', 'Nicholas Sparks', 1999, 150),
-('Angels and Demons', 'Thriller', 'Dan Brown', 2000, 150),
-('Ellon Musk', 'Biography', 'Ashley Vance', 2015, 150),
-('Gone Girl', 'Thriller', 'Gillian Flyn', 2012, 150),
-('Hidden Figures', 'Biography', 'Margot Lee Shetterly', 2016, 150),
-('If Tomorrow Comes', 'Drama', 'Sydney Sheldon', 1985, 150),
-('Long Walk to Freedom', 'Biography', 'Nelson Mandela', 1994, 150),
-('Pope Francis Untying the Knots', 'Biography', 'Paul Vallely', 2013, 150),
-('Pride and Prejudice', 'Romance', 'Jane Austen', 1813, 150),
-('Romeo and Juliet', 'Romance', 'William Shakespear', 1597, 150),
-('Seventh Plague', 'Drama', 'James Rollins', 2016, 150),
-('Stay Close', 'Drama', 'Harlan Coben', 2000, 150),
-('The Gift', 'Drama', 'James Patterson', 2010, 150),
-('The Notebook', 'ROmance', 'Nicholas Sparks', 1996, 150);
-
--- --------------------------------------------------------
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'A Human Element book 1','Thriller','Donna Gallanti',2012,150),(2,'A Human Element book 2','Thriller','Donna Galanti',2013,150),(3,'A Walk to Remember','Romance','Nicholas Sparks',1999,150),(4,'Angels and Demons','Thriller','Dan Brown',2000,150),(5,'Ellon Musk','Biography','Ashley Vance',2015,150),(6,'Gone Girl','Thriller','Gillian Flyn',2012,150),(7,'Hidden Figures','Biography','Margot Lee Shetterly',2016,150),(8,'If Tomorrow Comes','Drama','Sydney Sheldon',1985,150),(9,'Long Walk to Freedom','Biography','Nelson Mandela',1994,150),(10,'Pope Francis Untying the Knots','Biography','Paul Vallely',2013,150),(11,'Pride and Prejudice','Romance','Jane Austen',1813,150),(12,'Romeo and Juliet','Romance','William Shakespear',1597,150),(13,'Seventh Plague','Drama','James Rollins',2016,150),(14,'Stay Close','Drama','Harlan Coben',2000,150),(15,'The Gift','Drama','James Patterson',2010,150),(16,'The Notebook','Romance','Nicholas Sparks',1996,150);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(250) NOT NULL,
-  `username` varchar(250) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `email_2` (`email`),
+  UNIQUE KEY `phone` (`phone`),
+  UNIQUE KEY `phone_2` (`phone`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`role`, `username`, `email`, `password`) VALUES
-('', 'Doe', 'john@example.com', ''),
-('', '', '', '$2y$10$vF4mq8fNy9Rj.0aAwG20..CDflUZzvUze9J5wxcuiMdgLiokTmU8.'),
-('', '', '', '$2y$10$XVqzjLv.Zwgmab8hUFt9uOaTQkYTohQqLT7ldOfVA863g3xik0oKS'),
-('', '', '', '$2y$10$iVJaOH4AS38LEBu/qhvu7.eiFhvTuIda2yk3KAY1T4cyHyb4Iulty'),
-('', '', '', '$2y$10$kHXTy1/SxtqonSRxT8HOT.vRjVq3xnH88kjkB7Wu5n4EI/so8mxWS'),
-('', '', '', '$2y$10$8161PRyhNHeBsscekvcgmOLLIrdF3cofdcFwUeSoH3o3nhYb7x.P2'),
-('', '', '', '$2y$10$Ij.xoI0CqcS5u/palNbiwOz15ZSjxAxvVU1Y215DdlST.JHixZC7G'),
-('', '', '', '$2y$10$JtjaepGZw6ZCFPFHF4kKa.htQyfKlMTbTIM7cVKtn.FyrqlIcCsv6'),
-('', '', '', '$2y$10$/1H3wgAE6BYI78X3Sa2/tefPpM1HE8U/VKKH.CY0OTp/TNgxDUrTm'),
-('', '', '', '$2y$10$OEfD62lAbRJzADMnIcfHHuPVZp07PKLaqaBWa1NH7ay/dqDWPaeVK');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`title`);
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-26 20:19:59
